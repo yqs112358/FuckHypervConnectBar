030990a60c1055b1c62c1269f87ea6373d08f17c
yqs112358 <yqs112358@outlook.com>
2023-03-26 22:31:41 +0800

    Add source code

---
+++ ApiHooker/dllmain.cpp
@@ -0,0 +1,68 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+#include <detours.h>
+
+static BOOL (WINAPI* Real_ShowWindow)(HWND hWnd, int nCmdShow) = ShowWindow;
+
+wchar_t wndTitleBuf[MAX_PATH];
+wchar_t wndClassNameBuf[MAX_PATH];
+
+BOOL WINAPI hookedShowWindow(HWND hWnd, int nCmdShow)
+{
+    if (GetClassNameW(hWnd, wndClassNameBuf, sizeof(wndClassNameBuf) / sizeof(wchar_t)) == 0
+        || GetWindowTextW(hWnd, wndTitleBuf, sizeof(wndTitleBuf) / sizeof(wchar_t)) == 0)
+    {
+        return Real_ShowWindow(hWnd, nCmdShow);
+    }
+
+    if (wcscmp(wndClassNameBuf, L"BBarWindowClass") == 0 && wcscmp(wndTitleBuf, L"BBar") == 0)
+    {
+        // Hooked
+        // Hide it forever!
+        if (nCmdShow != SW_HIDE)
+            return TRUE;
+    }
+    return Real_ShowWindow(hWnd, nCmdShow);
+}
+
+void Hook()
+{
+    DetourRestoreAfterWith();
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    DetourAttach(&(PVOID&)Real_ShowWindow, hookedShowWindow);
+    DetourTransactionCommit();
+}
+
+void UnHook()
+{
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    DetourDetach(&(PVOID&)Real_ShowWindow, hookedShowWindow);
+    DetourTransactionCommit();
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(NULL, L"The connection bar of vmconnect.exe will be hidden forever.\n"
+            "If you need to go out of virtual machine, press \"Ctrl+Alt+←\".", L"vmconnect.exe", MB_OK);
+        Hook();
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        UnHook();
+        break;
+    }
+    return TRUE;
+}
+
---
+++ Launcher/launcher.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+#include <Psapi.h>
+#include <filesystem>
+#pragma comment (lib, "Psapi.lib")
+using namespace std;
+
+
+#define INJECTED_DLL_PATH "ApiHooker.dll"
+
+
+BOOL InjectDllToProcess(DWORD processId, PCSTR dllName)
+{
+    BOOL bOk = FALSE;
+    HANDLE hProcess = NULL, hThread = NULL;
+    PCSTR pszLibFileRemote = NULL;
+
+    do
+    {
+        hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION |
+            PROCESS_VM_WRITE, FALSE, processId);
+        if (hProcess == NULL)
+            break;
+
+        pszLibFileRemote = 
+            (PCSTR) VirtualAllocEx(hProcess, NULL, strlen(dllName) * sizeof(CHAR) + 1, MEM_COMMIT, PAGE_READWRITE);
+        if (pszLibFileRemote == NULL) 
+            break;
+        if (!WriteProcessMemory(hProcess, (LPVOID)pszLibFileRemote, (PVOID)dllName, strlen(dllName) * sizeof(CHAR) + 1, NULL))
+        {
+            break;
+        }
+
+        PTHREAD_START_ROUTINE pfnThreadRtn = 
+            (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle(TEXT("Kernel32")), "LoadLibraryA");
+        if (pfnThreadRtn == NULL) 
+            break;
+        hThread = CreateRemoteThread(hProcess, NULL, 0, pfnThreadRtn, (LPVOID)pszLibFileRemote, 0, NULL);
+        if (hThread == NULL)
+            break;
+
+        if (hThread != NULL)
+            CloseHandle(hThread);
+        if (hProcess != NULL)
+            CloseHandle(hProcess);
+        bOk = TRUE;
+    } 
+    while (FALSE);
+    return bOk;
+}
+
+bool EndsWith(const char* str, const char* suffix) {
+    if (!str || !suffix)
+        return 0;
+    size_t lenstr = strlen(str);
+    size_t lensuffix = strlen(suffix);
+    if (lensuffix > lenstr)
+        return 0;
+    return strncmp(str + lenstr - lensuffix, suffix, lensuffix) == 0;
+}
+
+DWORD GetProcessId(LPCSTR lpProcessName)
+{
+    DWORD dwProcessID[0x500] = { 0 };
+    DWORD dwNeeded = 0;
+    BOOL bEnumRes = EnumProcesses(dwProcessID, sizeof(dwProcessID), &dwNeeded);
+    UINT uCount = dwNeeded / sizeof(DWORD);
+    for (UINT i = 0; i < uCount; i++)
+    {
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwProcessID[i]);
+        if (NULL != hProcess)
+        {
+            CHAR szProcessName[MAX_PATH] = { 0 };
+            DWORD dwNameLen = MAX_PATH;
+            BOOL bRet = QueryFullProcessImageNameA(hProcess, 0, szProcessName, &dwNameLen);
+            if (EndsWith(szProcessName, lpProcessName))
+            {
+                return dwProcessID[i];
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    DWORD vmconnectPid = GetProcessId("vmconnect.exe");
+    if (vmconnectPid <= 0)
+    {
+        cout << "[ERROR] vmconnect.exe no found." << endl;
+        system("pause");
+        exit(1);
+    }
+    cout << "[INFO] Found vmconnect.exe, pid: " << vmconnectPid << endl;
+
+    std::error_code ec;
+    auto dllPath = std::filesystem::canonical(std::filesystem::path(INJECTED_DLL_PATH), ec);
+    if(ec)
+    {
+        cout << "[ERROR] " << INJECTED_DLL_PATH << " no found." << endl;
+        system("pause");
+        exit(1);
+    }
+    dllPath.make_preferred();
+    string dllPathStr = dllPath.generic_u8string();
+    cout << "[INFO] Inject DLL \"" << dllPathStr << "\" into vmconnect.exe..." << endl;
+
+    if (InjectDllToProcess(vmconnectPid, dllPathStr.c_str()))
+        cout << endl << "[INFO] Injected." << endl;
+    else
+        cout << endl << "[ERROR] Failed. Error code: " << GetLastError() << endl;
+	system("pause");
+	return 0;
+}
